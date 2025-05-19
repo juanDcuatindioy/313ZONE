cb6378d57c0cc0cfe53003e019db05c170755483
juanDcuatindioy <124367415+juanDcuatindioy@users.noreply.github.com>
2025-05-19 12:05:58 -0500

    conexion

--- server/index.js
+++ server/index.js
@@ -12,11 +12,11 @@ app.use(cors());
 app.use(express.json());
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  port: 3306,
+  host: process.env.DB_HOST || 'maglev.proxy.rlwy.net',
+  port: 21508,
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || '313zone_db',
+  password: process.env.DB_PASSWORD || 'dJaXCjgGfCatvikqmxOxvvHIhFBElHam',
+  database: process.env.DB_NAME || 'railway',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
